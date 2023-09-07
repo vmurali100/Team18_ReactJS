6119e0ed5238a3c609eae8a92e313562fdfb9557
vmurali100 <vmurali100@users.noreply.github.com>
2023-09-07 11:03:06 +0530

    Added Functional Components

--- ReactJS/app2/src/Components/Company.jsx
+++ ReactJS/app2/src/Components/Company.jsx
@@ -1,9 +1,23 @@
-import Employee from "./Employee"
+import Employee from "./Employee";
+import { useState } from "react";
 
 export const Company=()=>{
+    const [empName,setEmpName] = useState("Ram Kumar ....");
+    const [age,setAge]=useState(24);
+    const [email,setEmail]=useState("Ram@gmail.com")
+    // useState Hook 
+    // var empName = "Ravi Kumar";
+
+    const changeEmpName=()=>{
+        // empName = "Raghu Ram"
+        setEmpName("Raghu Ram");
+        setAge("43");
+        setEmail('RRRR@gmail.com')
+    }
     return <div>
         <h2>Welcome to Company Component !!</h2>
-        <Employee empName ={"Ram Krisna"} age ={24} email={"ram@gamil.com"}/>
+        <button onClick={changeEmpName}>Change Emp Name </button>
+        <Employee empName ={empName} age ={age} email={email}/>
         <Employee empName ={"Ravi Krisna"} age ={24} email={"ram@gamil.com"}/>
         <Employee empName ={"Sam Sundar"} age ={24} email={"ram@gamil.com"}/>
         <Employee empName ={"Sanjay Krisna"} age ={24} email={"ram@gamil.com"}/>
