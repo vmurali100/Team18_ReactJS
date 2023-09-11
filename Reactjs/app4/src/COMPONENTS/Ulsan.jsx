dd14ef0c2a7e9258606d570332d94f32a395daed
G.jahnavi <jahnavi2002g@gmail.com>
2023-09-11 15:11:30 +0530

    add

--- Reactjs/app4/src/COMPONENTS/Ulsan.jsx
+++ Reactjs/app4/src/COMPONENTS/Ulsan.jsx
@@ -6,6 +6,8 @@ export const Ulsan=()=>{
     const [age,setAge]=useState(54);
     const [email,setmail]=useState("SADDY@gmail.com")
     const [phno,setPhno]=useState(9494494949);
+    const [user,setUser] = useState({});
+    const [users,setUsers]=useState([])
    
     const changeEmpName=()=>{
         setEmpname("sanvi");
@@ -14,12 +16,24 @@ export const Ulsan=()=>{
         setPhno("0448385924");
     }
     
+    const addUserDetails=()=>{
+        setUser({
+            fname:"Ramya",
+            lname:"krishna"
+        })
+    }
+    const addusers=()=>{
+        setUsers(["Ravi","Kiran","Kumar"])
+    }
     return <div>
         <h2>Welcome to Company Component</h2>
         <button onClick={changeEmpName}>Change</button>
+        <button onClick={addUserDetails}>Add User Details</button> <br />
+        <button onClick={addusers}>Add Users</button>
         <Yeosu empname ={empName} age ={age} mail={email} phno={phno}/>
         <Yeosu empname ={"ramya"} age ={21} mail={"ramya@gamil.com"}/>
         <Yeosu empname ={"sravya"} age ={29} mail={"sravya@gamil.com"}/>
         <Yeosu empname ={"anjali"} age ={28} mail={"anjali@gamil.com"}/>
     </div>
 }
+
