6e10b6e092b31ee43250f4db254a1f49e3e03e50
vmurali100 <vmurali100@users.noreply.github.com>
2023-10-02 11:49:32 +0530

    Added

---
+++ Interview_Preparation_JS/Modules/app.js
@@ -0,0 +1,8 @@
+import { addNumbers, multiplyNumbers } from "./calculate.js";
+
+
+var total= addNumbers(20,50);
+console.log(total)
+
+var mutiplyValue = multiplyNumbers(30,20);
+console.log(mutiplyValue)
