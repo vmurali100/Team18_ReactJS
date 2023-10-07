40772dd23b942a698f5381d6b86f2970b683882a
vmurali100 <vmurali100@users.noreply.github.com>
2023-10-07 11:11:35 +0530

    Added

--- ReactJS/app4/src/App.js
+++ ReactJS/app4/src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Student from "./Student";
 import { useState } from "react";
+import Person from "./Person";
+import Employee from "./Employee";
 
 function App() {
   var [person, setPerson] = useState("Welcome to ReactJS"); // Creating a State
@@ -9,8 +11,10 @@ function App() {
   // var [firstNum,secondNum,thirdNum] = giveNumbers();
   return (
     <div>
+      <Employee/>
       {/* displaying the State Variables  */}
-      <h2>{person}</h2>
+      {/* <h2>{person}</h2> */}
+      {/* <Person/> */}
     </div>
   );
 }
