619bd2561e7a2a6090cccd375bbab5fcea2db084
vmurali100 <vmurali100@users.noreply.github.com>
2023-10-25 12:51:06 +0530

    Form Added

--- ReactJS/app6-react-forms/src/User.jsx
+++ ReactJS/app6-react-forms/src/User.jsx
@@ -7,44 +7,77 @@ export default class User extends Component {
     this.state = {
       fname: "",
       lname: "",
+      users: [],
     };
   }
 
   handleChange = (e) => {
-    console.log(e.target.name)
-    let proName = e.target.name
-    this.setState({[proName]:e.target.value})
+    console.log(e.target.name);
+    let proName = e.target.name;
+    this.setState({ [proName]: e.target.value });
   };
 
-  addUser=()=>{
-   let user={
-    fname:this.state.fname,
-    lname:this.state.lname
-   }
-   console.log(user)
-  }
+  addUser = () => {
+    let user = {
+      fname: this.state.fname,
+      lname: this.state.lname,
+    };
+    let newUsers = [...this.state.users];
+    newUsers.push(user);
+    this.setState({users:newUsers,fname:"",lname:""})
+    console.log(user);
+  };
 
   render() {
     return (
-      <form>
-        <label htmlFor="">First Name : </label>
-        <input
-          type="text"
-          name="fname"
-          onChange={this.handleChange}
-          value={this.state.fname}
-        />
-        <br />
-        <label htmlFor="">Last Name : </label>
-        <input
-          type="text"
-          name="lname"
-          onChange={this.handleChange}
-          value={this.state.lname}
-        />{" "}
-        <br />
-        <button type="button" onClick={this.addUser}>Get Value</button>
-      </form>
+      <div>
+        <form>
+          <label htmlFor="">First Name : </label>
+          <input
+            type="text"
+            name="fname"
+            onChange={this.handleChange}
+            value={this.state.fname}
+          />
+          <br />
+          <label htmlFor="">Last Name : </label>
+          <input
+            type="text"
+            name="lname"
+            onChange={this.handleChange}
+            value={this.state.lname}
+          />{" "}
+          <br />
+          <button type="button" onClick={this.addUser}>
+            Get Value
+          </button>
+        </form>
+
+        <table border={1}>
+          <thead>
+            <tr>
+              <th>First Name </th>
+              <th>Last  Name</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.users.map((usr)=>{
+              return <tr>
+                <td>{usr.fname}</td>
+                <td>{usr.lname}</td>
+                <td>
+                  <button>Edit</button>
+                </td>
+                <td>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
