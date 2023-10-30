0f3c9621ab2db4d2bb59b3150295c847f24a5374
evanithareddy999 <evanithareddy999@gmail.com>
2023-10-30 12:18:08 +0530

    added

---
+++ Reactjs/app8/src/Student.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+
+export default class 
+ extends Component {
+  render() {
+    return (
+      <div>
+        <form >
+          <label for="Customer care">University:</label>
+          <input
+            type="text"
+            id="university"
+            name="university"
+            required
+            onChange={this.handleChange}
+            value={this.state.student.university}
+          />
+          <br />
+          <br />
+          <label for="Part Name">Institute:</label>
+          <input
+            type="text"
+            id="institute"
+            name="institute"
+            required
+            onChange={this.handleChange}
+            value={this.state.student.institute}
+          />
+          <br />
+          <br />
+          <label for="Partprint Number">Branch:</label>
+          <input
+            type="text"
+            id="branch"
+            name="branch"
+            required
+            onChange={this.handleChange}
+            value={this.state.student.branch}
+          />
+          <br />
+          <br />
+          <label for="Revision">Degree:</label>
+          <input
+            type="text"
+            id="degree"
+            name="degree"
+            required
+            onChange={this.handleChange}
+            value={this.state.student.degree}
+          />
+          <br />
+          <br />
+          <label for="Part Number">Status:</label>
+          <input
+            type="text"
+            id="degree"
+            name="degree"
+            required
+            onChange={this.handleChange}
+            value={this.state.student.degree}
+          />
+         
+          <label for="Date of  Estimation">Status:</label>
+          <input
+            type="Date"
+            id="degree"
+            name="degree"
+            required
+            onChange={this.handleChange}
+            value={this.state.student.degree}
+          />
+         
+          <br />
+          <br />
+          <label for="Estimator Name">Status:</label>
+          <input
+            type="Date"
+            id="degree"
+            name="degree"
+            required
+            onChange={this.handleChange}
+            value={this.state.student.degree}
+          />
+          <br />
+          <br />
+          <label for="Buyer Name">Experience:</label>
+          <label for="Date of  Estimation">Status:</label>
+          <input
+            type="Date"
+            id="degree"
+            name="degree"
+            required
+            onChange={this.handleChange}
+            value={this.state.student.degree}
+          ></input>
+          <br />
+          <br />
+          
+         
+          <input type="button" value="Submit"  onClick={this.handleSubmit}/>
+        </form>
+
+        <hr />
+        <StudentsTable students={this.state.allStudents}/>
+      </div>
+    );
+  }
+}
+
+
