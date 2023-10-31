4ca033cd4f279af48cb9b2fe73805193375911dc
shanthicute <shanthicute.b1@gmail.com>
2023-10-31 15:33:26 +0530

    added

--- Reactjs/app11/src/App.js
+++ Reactjs/app11/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Form1 } from './components/Form1';
+import { Form2 } from './components/Form2';
 
 function App() {
   return (
     <div className="App">
      <Form1/>
+     <Form2/>
     </div>
   );
 }
---
+++ Reactjs/app11/src/components/Form2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Form2 = () => {
+  return (
+    <div>
+        <h1>student details</h1>
+        <form action="">
+            <label htmlFor="name">student name:</label>
+            <input type="text" id='name' />
+            <label htmlFor="lname"> father name:</label>
+            <input type="text" id='lname' />
+            
+        </form>
+    </div>
+  )
+}
