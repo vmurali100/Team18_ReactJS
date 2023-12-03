0c61991393bebfdaed1ed37503d700d7d2984eea
G.jahnavi <jahnavi2002g@gmail.com>
2023-12-03 13:51:05 +0530

    add

---
+++ Reactjs/app13/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Bana from './Bana';
+import Bana1 from './Bana1';
+import Bana2 from './Bana2';
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Header</h1>
+     <hr></hr>
+     <Router>
+     <ul>
+      <li><Link to={''}>Home</Link></li>
+      <li><Link to={'gallery'}>Gallery</Link></li>
+      <li><Link to={'contact'}>Contactus</Link></li>
+     </ul>
+      <Routes>
+        <Route path="/" element={<Bana/>}/>
+        <Route path="/gallery"  element={<Bana1/>}/>
+        <Route path="/contact"  element={<Bana2/>}/>
+      </Routes>
+     </Router>
+    <hr></hr>
+    <h1>Footer</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Reactjs/app13/src/Bana2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Bana2() {
+  return (
+    <div>
+        <h1>Contact  US Component</h1>
+    </div>
+  )
+}
