9816bc89deec8808b0698853404f5e41d573d1a1
evanithareddy999 <evanithareddy999@gmail.com>
2023-12-07 12:12:58 +0530

    code added

--- Reactjs/app11/src/App.js
+++ Reactjs/app11/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { Gallery } from './Gallery';
+import { Contact } from './Contact';
+import About from './About';
+import{BrowserRouter as Router,Link,Route,Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Header</h1>
+      <hr/>
+      <Router>
+<ul>
+  <li><Link to={''}>Home</Link></li>
+  <li><Link to={'gallery'}>Gallery</Link></li>
+  <li><Link to={'contact'}>Contact</Link></li>
+  <li><Link to={'about'}>About</Link></li>
+</ul>
+
+        <Routes>
+          <Route path="/"  element={<Home/>}/>
+          <Route path="/gallery"  element={<Gallery/>}/>
+          <Route path="/contact"  element={<Contact/>}/>
+          <Route path="/about"  element={<About/>}/>  
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ Reactjs/app11/src/Gallery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Gallery = () => {
+  return (
+    <div>
+        <h1>Gallery</h1>
+    </div>
+  )
+}
