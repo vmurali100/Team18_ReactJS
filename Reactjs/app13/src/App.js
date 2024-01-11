b6148a027f304bc441fa19e5ef491eab669e56c6
evanithareddy999 <evanithareddy999@gmail.com>
2024-01-11 11:19:08 +0530

    code

--- Reactjs/app13/src/App.js
+++ Reactjs/app13/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Sample1 } from './components/Sample1';
 import ClassComponent from './components/ClassComponent';
+import { Sample2 } from './components/Sample2';
 
 function App() {
   return (
     <div className="App">
       {/* <Sample1/> */}
-      <ClassComponent/>
+      {/* <ClassComponent/> */}
+      <Sample2/>
     </div>
   );
 }
---
+++ Reactjs/app13/src/components/Sample2.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export const Sample2 = () => {
+ 
+    const [myName,setMyName]=useState("mokshith")
+  
+
+const changeName =()=> {
+    setMyName("mokshagna")
+}
+return(
+    <>
+     <button onClick={changeName}>changeName</button>
+     <h1>{myName}</h1>
+    </>
+   
+    
+)
+}
